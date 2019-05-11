be1095ddf0903dc8ebe21f05c4eb3fbff53efab8
Johan Toft <johan.toft@gmail.com>
2019-05-11 17:26:19 +0200

    Fix calculation of points for line
    Renders had some holes in the lines.
    Decrease the step size slightly
    
    TODO: add some tests for this function

--- lib/geometry/src/geometry.cpp
+++ lib/geometry/src/geometry.cpp
@@ -9,14 +9,14 @@ namespace geometry
 
         std::vector<Point> points(numpoints);
 
-        float x_step = (p2.x - p1.x)/(numpoints - 1.0F);
-        float y_step = (p2.y - p1.y)/(numpoints - 1.0F);
+        float x_step = (p2.x - p1.x)/(numpoints);
+        float y_step = (p2.y - p1.y)/(numpoints);
 
         Point p = p1;
         for(int i = 0; i < numpoints; i++)
         {
-            Point result = {p.x, p.y};
-            points[i] = result;
+            Point result = {std::round(p.x), std::round(p.y)};
+            points.push_back(result);
             p.x += x_step;
             p.y += y_step;
         }
