2d6f7bd35c5eea75b3c451a13324d84f6df419b8
Johan Toft <johan.toft@gmail.com>
2019-05-11 14:44:10 +0200

    Remove start and end calculation
    It is not needed since the step will be
    positive or negative depending on the order
    of the points anyway.

--- src/main.cpp
+++ src/main.cpp
@@ -18,15 +18,12 @@ Line generate_line(const Point &p1, const Point &p2)
 {
     const int numpoints = std::max(std::fabs(p1.x-p2.x), std::fabs(p1.y-p2.y)) + 1;
 
-    const Point& start = p1.x < p2.x ? p1 : p2;
-    const Point& end = p1.x < p2.x ? p2 : p1;
-
     std::vector<Point> points(numpoints);
 
-    float x_step = (end.x - start.x)/(numpoints - 1.0F);
-    float y_step = (end.y - start.y)/(numpoints - 1.0F);
+    float x_step = (p2.x - p1.x)/(numpoints - 1.0F);
+    float y_step = (p2.y - p1.y)/(numpoints - 1.0F);
 
-    Point p = start;
+    Point p = p1;
     for(int i = 0; i < numpoints; i++)
     {
         Point result = {p.x, p.y};
