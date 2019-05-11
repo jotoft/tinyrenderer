a68d645997590577489878ccd2d0df5ed94f264c
Johan Toft <johan.toft@gmail.com>
2019-05-11 13:25:16 +0200

    Tinyrenderer project
    Add tga image class from https://github.com/ssloy/tinyrenderer/tree/909fe20934ba5334144d2c748805690a1fa4c89f
    
    Add a main program that uses the library and creates
    an output.tga image

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <tgaimage/tgaimage.h>
+
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
+
+int main(int argc, char **argv)
+{
+    TGAImage image(100, 100, TGAImage::RGB);
+    image.set(52, 41, red);
+    image.flip_vertically();
+    image.write_tga_file("output.tga");
+    return 0;
+}
